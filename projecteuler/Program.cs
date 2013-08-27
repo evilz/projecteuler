1c5b99746efa4899df89f5c69f4226fe7de56adf
Vincent Bourdon <evilznet@gmail.com>
2013-08-27 16:09:17 +0200

    3 first problems

---
+++ projecteuler/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projecteuler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //var result = problem1.GetSum();
+            //Console.WriteLine(result);
+
+
+            //var result = problem2.GetFiboSum();
+            //Console.WriteLine(result);
+
+            var result = problem3.GetLargetPrimeFactor();
+            Console.WriteLine(result);
+
+
+            Console.Read();
+
+        }
+    }
+}
---
+++ projecteuler/problem1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projecteuler
+{
+    public class problem1
+    {
+
+        //If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+        //Find the sum of all the multiples of 3 or 5 below 1000.
+
+        public static int GetSum()
+        {
+            List<int> numbers = new List<int>();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                if(i % 3 == 0 || i % 5 == 0)
+                    numbers.Add(i);
+            }
+
+
+           return numbers.Sum();
+
+
+
+        }
+
+    }
+}
